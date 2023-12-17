6fde31db93230afca40f37f0ae3580b1da5bff13
unknown <aadityad203@gmail.com>
2023-12-17 15:09:39 +0530

    first phase

--- script.js
+++ script.js
@@ -1,15 +1,87 @@
-const body = document.getElementById('body')
-console.log(body)
+// const bodyx = document.getElementById('body')
+// console.log(body)
+const svg = document.getElementById('svg')
+let index = 0;
 
 body.addEventListener('click', (e)=>{
     console.log('clicked the div')
-    console.log(e)
-    console.log(e.clientX + ' ' + e.clientY)  //i think this gives the absolute position of the mouse on the screen
+    // console.log(e)
+    // console.log(e.clientX + ' ' + e.clientY)  //i think this gives the absolute position of the mouse on the screen
     let div = document.createElement('div')
-    div.className = 'node'
+    // let node = document.createElement('circle')
+    const node = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+
+    let posX = ((e.pageX)/1500)*1500 
+    let posY = ((e.pageY)/892)*892
+    posX = (e.pageX/window.innerWidth) * 1500
+    posY = (e.pageY/window.innerHeight) * 892
+    // div.className = 'node'
+    // node.setAttribute('cx', e.pageX)
+    // node.setAttribute('cy', e.pageY)
+    node.setAttribute('cx', e.pageX)
+    node.setAttribute('cy', e.pageY)
+    node.setAttribute('r', '13')
+    node.setAttribute('class', 'node')
+    // console.log(e.pageX)
+    // console.log(e.pageY)
+    console.log(e.pageX+', '+e.pageY)
+    // console.log(e.pageY)
+
+    
+    node.setAttribute('id', 'node'+index)
+    index++
+    
     // div.style.position = 'relative'
     div.style.left = e.pageX+"px"
     div.style.top = e.pageY+"px"
-    console.log(div)
-    body.appendChild(div)
+    // console.log(div)
+    // document.body.appendChild(div)
+    // document.body.appendChild(node)
+    svg.appendChild(node)
+    // console.log(document.getElementById('node1'))
+    drawLines()
 })
+
+const drawLines = () =>{
+    const nodes = document.getElementsByClassName('node')
+    // console.table(nodes)
+    // Array.of(nodes).forEach((node, index) => {
+    //     console.log(node)
+    //     console.log(index)
+    // })
+    // console.log(nodes[0])
+    // [...nodes].forEach(node => {
+    //     console.log(node, index)
+        
+    // });
+    // console.log(nodes[0].getAttribute('cx'))
+
+    for(let i=0; i<nodes.length; i++){
+        let node1 = nodes[i]
+        let position1 = node1.getBoundingClientRect()
+        for(let j = i+1; j<nodes.length; j++){
+            let node2 = nodes[j]
+            let position2 = node2.getBoundingClientRect()
+            // console.log(node1)
+            // console.log(position1.left)
+            // console.log(node2)
+            // let line = document.createElement('svg')
+            let line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+
+            line.id = i+"-"+j
+            line.setAttribute('x1', node1.getAttribute('cx'))
+            line.setAttribute('x2', node2.getAttribute('cx'))
+            line.setAttribute('y1', node1.getAttribute('cy'))
+            line.setAttribute('y2', node2.getAttribute('cy'))
+
+            line.setAttribute('stroke', 'black')
+            line.setAttribute('stroke-width', '4')
+            
+
+            console.log(line)
+            svg.appendChild(line)
+            // console.log(nodes[i].getAttribute('pageX'))
+            // line.attr('x1', nodes[i].getAttribute('pageX'))
+        }
+    }
+}
